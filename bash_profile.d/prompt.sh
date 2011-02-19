64c1eccc7343bbea220534bc4d01b89b7dd01d39
Darren Boyd <darren.boyd@gmail.com>
2011-02-19 14:26:04 -0800

    Awesome git mode display and a fixed specific revision checkout.
    Conflicts:
    
    	bash_profile.d/prompt.sh

--- bash_profile.d/prompt.sh
+++ bash_profile.d/prompt.sh
@@ -1,5 +1,5 @@
 if [ $SSH_TTY ]; then
-  PS1="$RED\u@\h $LIGHTGRAY\w $BROWN\$(current_git_branch)$YELLOW\$(git_modifications)$BLUE$ $NC"
+  PS1="$RED\u@\h $LIGHTGRAY\w $BROWN\$(current_git_branch)$YELLOW\$(git_special)$BLUE$ $NC"
 else
-  PS1="$GREEN\h $LIGHTGRAY\w $BROWN\$(current_git_branch)$YELLOW\$(git_modifications)$BLUE$ $NC"
+  PS1="$GREEN\h $LIGHTGRAY\w $BROWN\$(current_git_branch)$YELLOW\$(git_special)$BLUE$ $NC"
 fi

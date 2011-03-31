48c4e61ea6ea2970dc7b5c7f0433937c2ef88f52
Darren Boyd <dboyd@realgravity.com>
2011-03-31 16:39:27 -0700

    Removing the 'gi' git init alias.
    I type this far too often, by accident.

--- bash_profile.d/git.sh
+++ bash_profile.d/git.sh
@@ -1,7 +1,4 @@
 
-alias gi='git init && printf ".DS_Store\nThumbs.db\n" >> .gitignore && git add .gitignore'
-
-
 # http://www.jukie.net/~bart/blog/pimping-out-git-log
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%an %cr)%Creset' --abbrev-commit --date=relative"
 alias glp='gl -p'

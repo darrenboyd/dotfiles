aa82010c6ba712dad7500da723d4a4a9ef305d48
Darren Boyd <dboyd@realgravity.com>
2011-12-20 12:25:25 -0800

    Adding textmate bash profile file.

---
+++ bash_profile.d/textmate.sh
@@ -0,0 +1,3 @@
+# http://blog.macromates.com/2007/file-type-detection-rspec-rails/
+alias textmate_manual_bindings='defaults read com.macromates.textmate OakLanguageFileBindings'
+alias textmate_clear_manual_bindings='defaults delete com.macromates.textmate OakLanguageFileBindings'

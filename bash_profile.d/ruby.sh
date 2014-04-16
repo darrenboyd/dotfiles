8dd6090dc1bccf05c16ad3566bc9ce236eb5a8c0
Darren Boyd <darren@blujack.com>
2014-04-16 08:17:30 -0700

    Cleanup rails/ruby aliases and commands.

--- bash_profile.d/ruby.sh
+++ bash_profile.d/ruby.sh
@@ -2,45 +2,11 @@
 
 export BUNDLER_EDITOR="mvim"
 
-rails_root() {
-  (
-    dir=${1:-$(pwd)}
-    i=0
-    while [ "/" != "$dir" -a "$i" -ne 16 ]; do
-      if [ -f "$dir/config/environment.rb" ]; then
-        echo "$dir"
-        return 0
-      fi
-      dir="$(dirname "$dir")"
-      i=$(expr $i + 1)
-    done
-    echo "* Does not appear to be a Rails project" >&2
-    return 1
-  )
-}
-
-
-script_rails() {
-  rails_root=`rails_root`
-  if [ -f "$rails_root/script/rails" ]; then
-    "$rails_root/script/rails" "$@"
-  elif [ -f "$rails_root/bin/rails" ]; then
-    "$rails_root/bin/rails" "$@"
-  elif [ -f "$rails_root/script/$1" ]; then
-    local name
-    name="$1"
-    shift
-    "$rails_root/script/$name" "$@"
-  else
-    return 1
-  fi
-}
-
-alias rc='script_rails console'
-alias rg='script_rails generate'
-alias rd='script_rails destroy'
-alias rs='script_rails server'
-alias rdb='script_rails dbconsole -p'
+alias rc='bin/rails console'
+alias rg='bin/rails generate'
+alias rd='bin/rails destroy'
+alias rs='bin/rails server'
+alias rdb='bin/rails dbconsole -p'
 
 function fs() {
   if [ -f Procfile.dev ]; then
@@ -63,11 +29,6 @@ alias br='bundle exec rake'
 alias t='time testrb -Itest'
 alias cuc='bundle exec cucumber -P --format pretty --quiet'
 
-alias cdgems='cd `rvm gemdir`'
-
-alias rak='ack --ruby --cucumber'
-alias wak='ack --web'
-alias rak-no-test='ack --ignore-dir=test --ruby'
 alias rgrep='grep -rnH'
 
 # decent MRI GC settings for faster local development/test

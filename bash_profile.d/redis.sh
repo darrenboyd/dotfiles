16e7059c01795d7de67b304fe9ea5e5d87165bff
Darren Boyd <darren.boyd@gmail.com>
2015-03-30 08:02:18 -0700

    Updated redis start with daemonize

--- bash_profile.d/redis.sh
+++ bash_profile.d/redis.sh
@@ -1,2 +1,2 @@
-alias redis-start="redis-server /usr/local/etc/redis.conf"
+alias redis-start="redis-server /usr/local/etc/redis.conf --daemonize yes"
 alias redis-stop='kill `cat /usr/local/var/run/redis.pid`'

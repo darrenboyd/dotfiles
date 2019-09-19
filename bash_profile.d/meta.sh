b3c84d40a32bf34aa9fb09c3e06b0f2369506e8d
Darren Boyd <darren@lendeavor.com>
2019-09-19 09:13:28 -0700

    Update metamorphsum support

--- bash_profile.d/meta.sh
+++ bash_profile.d/meta.sh
@@ -1,10 +1,16 @@
 function meta() {
-  curl --silent http://metaphorpsum.com/paragraphs/$@ | xsel --clipboard ;
-}
+  p=$1
+  s=$2
 
-function metas() {
-  curl --silent http://metaphorpsum.com/sentences/$@ | xsel --clipboard ;
+  if [ -z "$p" ]; then
+    p=1
+  fi
+
+  if [ -z "$s" ]; then
+    s=4
+  fi
+
+  curl --silent http://metaphorpsum.com/paragraphs/$p/$s | xsel --clipboard ;
 }
 
 declare -fx meta
-declare -fx metas

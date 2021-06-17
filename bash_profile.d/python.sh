38689a2d1e73c3d28cc348e54071dc6345c2ae89
Darren Boyd <darren@lendeavor.com>
2021-06-17 10:06:28 -0700

    Cleanup around asdf

--- bash_profile.d/python.sh
+++ bash_profile.d/python.sh
@@ -1,13 +1,4 @@
 
-#alias python=python3
-
-# export PYENV_ROOT="$HOME/.pyenv"
-# export PATH="$PYENV_ROOT/bin:$PATH"
-
-# if `command -v pyenv 1>/dev/null 2>&1`; then
-#    eval "$(pyenv init -)"
-# fi
-
 if [ `which aws_completer` ]; then
   complete -C $(which aws_completer) aws
 fi

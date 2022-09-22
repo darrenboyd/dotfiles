0d8ee9d5849d4dcf2f91c1809d4499d8e17878e5
Darren Boyd <darren.boyd@gmail.com>
2022-09-22 13:59:54 -0700

    Some improvements to dev aliases

--- bashrc.d/git.sh
+++ bashrc.d/git.sh
@@ -1,6 +1,6 @@
 
 # https://gist.github.com/igrigorik/6666860
-alias gh="o \`git remote -v | grep git@github.com | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/https:\/\//'\`"
+alias gho="o \`git remote -v | grep git@github.com | grep fetch | head -1 | cut -f2 | cut -d' ' -f1 | sed -e's/:/\//' -e 's/git@/https:\/\//'\`"
 
 # http://www.jukie.net/~bart/blog/pimping-out-git-log
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%an %cr)%Creset' --abbrev-commit --date=relative"
--- bashrc.d/ruby.sh
+++ bashrc.d/ruby.sh
@@ -1,20 +1,12 @@
 # Inspiration from http://github.com/hashrocket/dotmatrix/blob/master/.hashrc
 
-alias rc='spring rails console'
-alias rg='spring rails generate'
-alias rd='spring rails destroy'
-alias rs='spring rails server'
-alias rdb='spring rails dbconsole -p'
+alias rc='rails console'
+alias rg='rails generate'
+alias rd='rails destroy'
+alias rs='rails server'
+alias rdb='rails dbconsole -p'
 
-alias ss='spring stop'
-
-function os() {
-  if [ -f Procfile.dev ]; then
-    overmind start -f Procfile.dev $*
-  else
-    overmind start $*
-  fi
-}
+#alias ss='spring stop'
 
 function os() {
   if [ -f Procfile.dev ]; then
@@ -24,14 +16,16 @@ function os() {
   fi
 }
 
-alias dm='spring rake db:migrate'
-alias dmt='spring rake db:test:prepare'
-alias dmu='spring rake db:rollback'
-alias dmr='spring rake db:migrate:redo'
+alias dm='rake db:migrate'
+alias dmt='rake db:test:prepare'
+alias dmu='rake db:rollback'
+alias dmr='rake db:migrate:redo'
 
 alias gua='bundle exec guard'
 
 alias tt='[ -e log/test.log ] && cp -f /dev/null log/test.log && tail -f log/test.log'
+
+alias rup='git up; bundle install; yarn install; rake db:migrate'
 
 # decent MRI GC settings for faster local development/test
 export RUBY_GC_MALLOC_LIMIT_MAX=90000000
